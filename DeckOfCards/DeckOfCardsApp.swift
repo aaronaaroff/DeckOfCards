4e1676284f654e6616361b43fbb684c7fedddebe
aaronaaroff <aaronje13@gmail.com>
2025-04-14 13:50:54 -0700

    Initial Commit

---
+++ DeckOfCards/DeckOfCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckOfCardsApp.swift
+//  DeckOfCards
+//
+//  Created by Iceman on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DeckOfCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeckOfCardsTests/DeckOfCardsTests.swift
@@ -0,0 +1,16 @@
+//
+//  DeckOfCardsTests.swift
+//  DeckOfCardsTests
+//
+//  Created by Iceman on 4/14/25.
+//
+
+import Testing
+
+struct DeckOfCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
